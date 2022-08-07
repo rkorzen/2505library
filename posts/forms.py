ab10d8f42a648da3a13716d29c76e73b1c8260ba
rkorzen <korzeniewski@gmail.com>
2022-08-07 14:59:58 +0200

    ModelForm

--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 
+from posts.models import Post
+
 
 class PostForm(forms.Form):
     title = forms.CharField()
     content = forms.CharField(widget=forms.Textarea)
 
+
+class PostForm2(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ["title", 'content',]
--- posts/views.py
+++ posts/views.py
@@ -3,34 +3,35 @@ from comments.forms import CommentForm
 
 # Create your views here.
 from comments.models import CommentPost
-from posts.forms import PostForm
+from posts.forms import PostForm, PostForm2
 from posts.models import Post
 
 
 def listview(request):
     posts = Post.objects.all()
-    print(request.method)
-    print(request.user)
-    print(request.user.is_authenticated)
+
     if request.method == "POST":
-        title = request.POST.get("title")
-        content = request.POST.get("content")
-        print(request.user)
-        print(type(request.user))
-        post = Post.objects.create(
-            title=title,
-            content=content,
-            author=request.user
-        )
+        form = PostForm2(data=request.POST)
+
+        # title = request.POST.get("title")
+        # content = request.POST.get("content")
+        # print(request.user)
+        # print(type(request.user))
+        # post = Post.objects.create(
+        #     title=title,
+        #     content=content,
+        #     author=request.user
+        # )
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
 
     return render(
         request,
         "posts/list.html",
-        {"posts": posts,
-
-         "x": "123",
-         "y": "ccdcdc",
-         }
+        {"posts": posts, 'post_form': PostForm2()}
     )
 
 
@@ -40,11 +41,13 @@ def details(request, post_id):
     if request.method == "POST":
         print(request.POST)
         if 'post_submit' in request.POST:
-            post_form = PostForm(request.POST)
+            post_form = PostForm2(request.POST, instance=post)
             if post_form.is_valid():
-                post.title = post_form.cleaned_data['title']
-                post.content = post_form.cleaned_data['content']
-                post.save()
+                # post.title = post_form.cleaned_data['title']
+                # post.content = post_form.cleaned_data['content']
+                # post.save()
+                post_form.save()
+
         elif 'comment_submit' in request.POST:
             comment_form = CommentForm(request.POST)
             if comment_form.is_valid():
@@ -53,7 +56,7 @@ def details(request, post_id):
                 comment = CommentPost.objects.create(**data)
 
     data = {'title': post.title, "content": post.content}
-    post_form = PostForm(data=data)
+    post_form = PostForm2(data=data)
     comment_form = CommentForm()
 
     return render(
